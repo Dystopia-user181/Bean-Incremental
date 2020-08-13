5d7300ac9b953349ed473da9edb3fc2feaaf099e
The-MathsMaster <chemistryfrancium@gmail.com>
2020-08-13 10:56:42 +0800

    quick bugfix

--- js/game/core/shrinkpow.js
+++ js/game/core/shrinkpow.js
@@ -6,8 +6,8 @@ function buyGen() {
 function buyGenMax() {
 	if (player.cmpcbean.lt(Decimal.pow(2, player.shrinkers).mul(2))) return;
 	var shrinkeramt = new Decimal(Math.floor(player.cmpcbean.log(2))).sub(player.shrinkers);
-	player.shrinkers = player.shrinkers.add(shrinkeramt);
 	player.cmpcbean = player.cmpcbean.sub(Decimal.pow(2, player.shrinkers.add(shrinkeramt)));
+	player.shrinkers = player.shrinkers.add(shrinkeramt);
 }
 setInterval(function () {
 	player.shrinkpow = player.shrinkpow.add(player.shrinkers.div(100));
